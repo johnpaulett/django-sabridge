7c842de7b28a410555d2eeb114c4cd6584a05d65
John Paulett <john@paulett.org>
2011-07-04 08:45:18 -0500

    If pre-release, provide status in version number.

--- sabridge/version.py
+++ sabridge/version.py
@@ -1,6 +1,9 @@
 VERSION = (0, 1, 0, 'dev')
 
 def get_version():
-    return '%s.%s.%s%s' % VERSION
+    base = '.'.join((str(x) for x in VERSION[0:3]))
+    if VERSION[3] == 'final':
+        return base
+    return '%s%s' % (base, VERSION[3])
 
 __version__ = get_version()
